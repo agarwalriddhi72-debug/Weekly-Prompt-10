efebacdf0b427ad5287139161760e599e2217bce
Riddhi <agarwal.riddhi72@gmail.com>
2026-03-27 22:14:44 -0400

    Weekly Prompt 10

---
+++ WP10.py
@@ -0,0 +1,12 @@
+"""
+Testing code is beneficial in many ways. 
+It helps improve the code by making it not only more clear, but also more reliable and easier to maintain. 
+Testing basically forces you to think about what might go wrong. 
+Sometimes we can write simple functions but forget to handle the "weird" inputs. 
+For example if a code prompts the user to enter a number from 1-5 but the user enters 6 then the code should prompt the user to enter a valid number. 
+However, without testing it might've not been so apparent to code an input validation. 
+Testing also helps clear up errors. 
+To test code, functions and code have to be more clear, because if we have a huge block of code that does multiple things then it becomes difficult to test. 
+So testing also forces you to break apart the code into smaller pieces making code way more organized. 
+Additionally, with the smaller bits of code and organized code it becomes easier to make changes in the code and also maintain it. 
+"""
